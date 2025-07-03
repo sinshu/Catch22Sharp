92b0bc9e5ade77fab6c33133fe0f737a0c0d06f1
Nobuaki Tanaka <sinshu.d@gmail.com>
2025-07-03 22:30:01 +0900

    Add test.

---
+++ Catch22SharpTest/DN_OutlierInclude.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Catch22Sharp;
+
+namespace Catch22SharpTest
+{
+    [TestClass]
+    public class DN_OutlierInclude
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var actual = Catch22.DN_OutlierInclude_n_001_mdrmd(TestData.Test1);
+            var expected = TestData.Test1Output["DN_OutlierInclude_n_001_mdrmd"];
+            Assert.AreEqual(expected, actual, 1.0E-6);
+        }
+    }
+}
---
+++ Catch22SharpTest/TestData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Catch22SharpTest
+{
+    [TestClass]
+    public class TestData
+    {
+        private static readonly string testDataDirectory = @"..\..\..\..\testData";
+
+        public static double[] Test1 = default!;
+        public static Dictionary<string, double> Test1Output = default!;
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext testContext)
+        {
+            Test1 = GetData(Path.Combine(testDataDirectory, "test.txt"));
+            Test1Output = GetExpectedValues(Path.Combine(testDataDirectory, "test_output.txt"));
+        }
+
+        [TestMethod]
+        public void Test1Data()
+        {
+            Assert.IsNotNull(Test1);
+            Assert.IsNotNull(Test1Output);
+        }
+
+        private static double[] GetData(string path)
+        {
+            return File.ReadLines(path).Select(double.Parse).ToArray();
+        }
+
+        private static Dictionary<string, double> GetExpectedValues(string path)
+        {
+            var dic = new Dictionary<string, double>();
+
+            foreach (var line in File.ReadLines(path))
+            {
+                if (line.Length == 0)
+                {
+                    break;
+                }
+
+                var split = line.Split(", ");
+                var name = split[1];
+                var value = double.Parse(split[0]);
+                dic.Add(name, value);
+            }
+
+            return dic;
+        }
+    }
+}
