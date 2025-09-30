9d22ccc1777b3b636894fe2e3784ba19fecc86ef
Nobuaki Tanaka <sinshu.d@gmail.com>
2025-09-30 10:30:24 +0900

    Add tests and normalize data for MD hrv classic pnn40

---
+++ Catch22SharpTest/MD_hrv_classic_pnn40.cs
@@ -0,0 +1,40 @@
+using Catch22Sharp;
+
+namespace Catch22SharpTest
+{
+    [TestClass]
+    public class MD_hrv_classic_pnn40
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var actual = Catch22.MD_hrv_classic_pnn40(TestData.Test1, TestData.Test1.Length);
+            var expected = TestData.Test1Output["MD_hrv_classic_pnn40"];
+            Assert.AreEqual(expected, actual, 1.0E-6);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var actual = Catch22.MD_hrv_classic_pnn40(TestData.Test2, TestData.Test2.Length);
+            var expected = TestData.Test2Output["MD_hrv_classic_pnn40"];
+            Assert.AreEqual(expected, actual, 1.0E-6);
+        }
+
+        [TestMethod]
+        public void TestShort()
+        {
+            var actual = Catch22.MD_hrv_classic_pnn40(TestData.TestShort, TestData.TestShort.Length);
+            var expected = TestData.TestShortOutput["MD_hrv_classic_pnn40"];
+            Assert.AreEqual(expected, actual, 1.0E-6);
+        }
+
+        [TestMethod]
+        public void TestSinusoid()
+        {
+            var actual = Catch22.MD_hrv_classic_pnn40(TestData.TestSinusoid, TestData.TestSinusoid.Length);
+            var expected = TestData.TestSinusoidOutput["MD_hrv_classic_pnn40"];
+            Assert.AreEqual(expected, actual, 1.0E-6);
+        }
+    }
+}
